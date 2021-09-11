b8579a6027e8fe4975e73b524da6c53457d0e4e8
efmanu <manuedavakandam@gmail.com>
2021-09-11 12:46:27 +1000

    precompile functions

--- run.jl
+++ run.jl
@@ -7,6 +7,6 @@ PrecompileFuncs.precompile_nn_train()
 
 app = FluxDash.make_app()
 
-# port = haskey(ENV, "PORT") ? parse(Int64, ENV["PORT"]) : 8050
+port = haskey(ENV, "PORT") ? parse(Int64, ENV["PORT"]) : 8050
 
-# FluxDash.Dash.run_server(app, "0.0.0.0", port)
+FluxDash.Dash.run_server(app, "0.0.0.0", port)
